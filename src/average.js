4d128b497af180e7c7228d2db9686d3ff329a156
zitherSide <takuyakotoyori@akane.waseda.jp>
2020-12-01 21:44:22 +0900

    practice1

---
+++ src/average.js
@@ -0,0 +1,8 @@
+const input = [80, 90, 100]
+const sum = (total, current) => total + current
+const total = arr => arr.reduce(sum)
+const size = arr => arr.length
+const divide = (a, b) => a / b
+const average = arr => divide(total(arr), size(arr))
+
+console.log(average(input))
