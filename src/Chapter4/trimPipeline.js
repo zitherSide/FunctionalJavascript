492a47e2f53a7362c77c2c6bc93062c09e1aafea
zitherSide <takuyakotoyori@akane.waseda.jp>
2020-12-09 19:00:48 +0900

    fixed Tuple.js

--- src/Chapter4/trimPipeline.js
+++ src/Chapter4/trimPipeline.js
@@ -1,6 +1,6 @@
 const { first } = require('lodash')
 var _ = require('lodash')
-//var Tuple = require('../Tuple')
+var Tuple = require('../Tuple')
 
 //trim :: string -> string
 const trim = s => s.replace(/^\s*|\s*$/g, '')
@@ -8,26 +8,6 @@ const trim = s => s.replace(/^\s*|\s*$/g, '')
 //normalize :: string -> string
 const normalize = s => s.replace(/-/g, '')
 
-const Tuple = function( /* types */){
-    const typeInfo = Array.prototype.slice.call(arguments, 0)
-    const _T = function(/* values */){
-        const values = Array.prototype.slice.call(arguments, 0)
-        if(values.some( val => val === null || val === undefined)){
-            throw new ReferenceError("Tuple may not have any null values")
-        }
-        if(values.length !== typeInfo.length){
-            throw new TypeError("Tuple arity does not match its prototype")
-        }
-        values.map( (val, index) => {
-            this[`_${index + 1}`] = checkType(typeInfo[index])(val)
-        }, this)
-        Object.freeze(this)
-    }
-    _T.prototype.values = () =>{
-        return Object.keys(this).map(k => this[k], this)
-    }
-    return _T
-}
 const Status = Tuple(Boolean, String)
 const isValid = function(str){
     if(str.length === 0){
@@ -41,5 +21,6 @@ console.log(isValid(normalize(trim('444-44-444'))))
 
 const StringPair = Tuple(String, String)
 const name = new StringPair('Barkley', 'Rosser')
-[first, last] = name.values()
-const fullname = new StringPair('J', 'Barkley', 'Rosser') //error
+//[first, last] = name
+console.log(name)
+//const fullname = new StringPair('J', 'Barkley', 'Rosser') //error
--- src/Tuple.js
+++ src/Tuple.js
@@ -1,18 +1,32 @@
+var R = require('ramda')
+
+const checkType = R.curry( (typeDef, obj) => {
+    if(!R.is(typeDef, obj)){
+        let type = typeof obj
+        throw new TypeError(`Type mismatch. Expected [${typeDef}] but found ${[type]}`)
+    }
+    return obj
+})
+
 const Tuple = function( /* types */){
-    const typeInfo = Array.prototype.slice.call(arguments, 0)
+    const typeInfo = Array.prototype.slice.call(arguments, 0)   //Tupleのコールで型情報を保持
     const _T = function(/* values */){
-        const values = Array.prototype.slice.call(arguments, 0)
+        const values = Array.prototype.slice.call(arguments, 0) //作ったTupleに対するコールで値を保持
         if(values.some( val => val === null || val === undefined)){
             throw new ReferenceError("Tuple may not have any null values")
         }
         if(values.length !== typeInfo.length){
             throw new TypeError("Tuple arity does not match its prototype")
         }
-        values.map(val, index => {
+        values.map((val, index) => {
             this[`_${index + 1}`] = checkType(typeInfo[index])(val)
         }, this)
         Object.freeze(this)
     }
+    _T.prototype.values = () => {
+        return Object.keys(this).map(k => this[k], this)
+    }
+    return _T
 }
 
-module.exports.Tuple = Tuple
+module.exports = Tuple
