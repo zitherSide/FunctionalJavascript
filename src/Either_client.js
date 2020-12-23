73183372b4cd5cf4410541a9053b543bf1788368
kotoyori.takuya <kotoyori.takuya>
2020-12-24 08:26:40 +0900

    Added either monad for client side.

---
+++ src/Either_client.js
@@ -0,0 +1,86 @@
+class Either{
+    constructor(value){
+        this._value = value
+    }
+    get value(){
+        return this._value
+    }
+    get isLeft(){
+        return false
+    }
+    get ifRight(){
+        return false
+    }
+    static left(a){
+        return new Left(a)
+    }
+    static right(a){
+        return new Right(a)
+    }
+    static fromNullable(val){
+        return val !== null && val !== undefined ? Either.right(val) : Either.left(val)
+    }
+    static of(a){
+        return Either.right(a)
+    }
+}
+
+class Left extends Either{
+    map(_){
+        return this
+    }
+    get value(){
+        throw new TypeError("Can't extract the value of a Left(a).")
+    }
+    get isLeft(){
+        return true
+    }
+    getOrElse(other){
+        return other
+    }
+    orElse(f){
+        return f(this._value)
+    }
+    chain(f){
+        return this
+    }
+    getOrElseThrow(a){
+        throw new Error(a)
+    }
+    filter(f){
+        return this
+    }
+    toString(){
+        return `Either.Left(${this._value})`
+    }
+}
+
+class Right extends Either{
+    map(f){
+        return Either.of(f(this._value))
+    }
+    get value(){
+        return this._value
+    }
+    get isRight(){
+        return true
+    }
+    getOrElse(_){
+        return this._value
+    }
+    orElse(_){
+        return this
+    }
+    chain(f){
+        return f(this._value)
+    }
+    getOrElseThrow(_){
+        return this._value
+    }
+    filter(f){
+        return Either.fromNullable(f(this._value) ?  this._value : null)
+    }
+    toString(){
+        return `Either.Right(${this._value})`
+    }
+}
